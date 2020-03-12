562f411e1ac6de3ac79a7cba5b853d3080051dbb
Arjun Raja <arjunraja@cybrilla.com>
2020-03-12 11:35:43 +0530

    working project

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ if (isMainThread) {
   const worker = new Worker('./worker_1.js');
   const worker2 = new Worker('./worker_2.js');
   worker.on('message', (message) => {
-    if(startTradeTime === null){
-      startTradeTime = message.TS2;
-    }  
-    readData = message;
-    readData.startTime = startTradeTime;
-    
-    worker2.postMessage(message);
+      if(startTradeTime === null){
+        startTradeTime = message.TS2;
+      }  
+      readData = message;
+      readData.startTime = startTradeTime;
+      worker2.postMessage(message);
+  });
+  worker.on('error', error => { console.log('err', error)});
+  worker.on('exit', () => { 
+    readComplete = true;
+    worker2.postMessage("XXBTZUSD");
   });
 } else {
   console.log(isMainThread); 
